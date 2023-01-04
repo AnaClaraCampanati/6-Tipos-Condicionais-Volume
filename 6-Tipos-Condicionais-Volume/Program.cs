841722904068444ab178fa35bb9d0123bdfa70b4
Ana Clara campanati <campanatianaclara@gmail.com>
2023-01-04 15:34:31 -0300

    Adicionar arquivos de projeto.

---
+++ 6-Tipos-Condicionais-Volume/Program.cs
@@ -0,0 +1,13 @@
+﻿// Calcular e apresentar o valor do volume de uma lata de óleo, utilizando a fórmula:
+// V = 3.14159 * R * R * A
+// Onde as variáveis: V, R e A representam respectivamente o volume, o raio e a altura.
+
+Console.WriteLine("Digite o valor do raio:");
+int raio = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite o valor da altura:");
+int altura = Convert.ToInt32(Console.ReadLine());
+
+double volume = 3.14159 * raio * raio *  altura;
+
+Console.WriteLine("Volume: " + volume);
